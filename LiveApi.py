d7771dcc609bf878dde5f49c1aebccbce690e5ab
alfamoch <alfamoch58@gmail.com>
2025-12-05 23:09:24 +0700

    feat: Load system instruction from file and update audio configuration settings

--- LiveApi.py
+++ LiveApi.py
@@ -22,18 +22,25 @@ os.environ["GEMINI_API_KEY"] = "AIzaSyCdlmDj1rJhq91RQwiw5F3rFyCFKzmbGmk"  # Repl
 
 client = genai.Client(http_options={"api_version": "v1alpha"})  # GEMINI_API_KEY must be set as env variable
 
-system_instruction = """
-You are a helpful and friendly AI assistant.
-Your default tone is helpful, engaging, and clear, with a touch of optimistic wit.
-Anticipate user needs by clarifying ambiguous questions and always conclude your responses
-with an engaging follow-up question to keep the conversation flowing.
-"""
+# Load system instruction from file
+with open("system_instruction.txt", "r") as f:
+    system_instruction = f.read()
 
 MODEL = "gemini-2.5-flash-native-audio-preview-09-2025"
 CONFIG = {
     "system_instruction": system_instruction,
     "response_modalities": ["AUDIO"],
-    "proactivity": {'proactive_audio': True},
+    "realtime_input_config": {
+        "automatic_activity_detection": {
+            "disabled": False, # default
+            "start_of_speech_sensitivity": "START_SENSITIVITY_HIGH",
+            "end_of_speech_sensitivity": "END_SENSITIVITY_LOW",
+            "prefix_padding_ms": 20,
+            "silence_duration_ms": 10 }},
+    "speech_config": {
+        "voice_config": {"prebuilt_voice_config": {"voice_name": "Charon"}}},
+    "enable_affective_dialog": False,
+    "proactivity": {'proactive_audio': False},
     "output_audio_transcription": {},
     "input_audio_transcription": {},
 }
